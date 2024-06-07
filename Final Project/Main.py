563c19eb3bee8384c74d57b8df83b62b18eefdfc
vky6366 <147257265+vky6366@users.noreply.github.com>
2024-06-07 07:06:12 +0530

    Update Main.py

--- Final Project/Main.py
+++ Final Project/Main.py
@@ -117,11 +117,10 @@ KV = '''
 Screen:
     FloatLayout:
         canvas.before:
-            Color:
-                rgba: 224/255, 255/255, 255/255, 1
             Rectangle:
-                size: self.size
+                source: "C:/Users/vishw/Desktop/Final Project/LockBG.jpg"
                 pos: self.pos
+                size: self.size
         MDRaisedButton:
             text: "Feedback"
             size_hint: None, None
@@ -135,29 +134,40 @@ Screen:
             pos_hint: {'center_x': 0.5, 'top': 0.85}
 
         MDCard:
-            background_normal: ''
-            background_color: 0, 0, 0, 0
+        background_normal: ''
+        background_color: 0.878, 1.0, 1.0, 0.5  # Set alpha to 0.5 for 50% opacity
+        size_hint: None, None
+        size: "600dp", "220dp"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.3}
+        elevation: 1
+        padding: "10dp"
+        spacing: "10dp"
+        orientation: 'vertical'
+    
+        Label:
+            text: 'Enter URL'
+            size_hint_y: None
+            height: 40
+            font_size: '18sp'
+            color: 0, 0, 0, 1
+            pos_hint: {'center_x': 0.5,'center_y': 0.8}
+    
+        MDTextField:
+            id: url_entry
+            hint_text: "Enter URL"
+            helper_text_mode: "on_focus"
+            icon_right: "magnify"
+            size_hint_x: None
+            width: "250dp"
+            pos_hint: {'center_x': 0.5}
+    
+        MDRaisedButton:
+            text: "Detect"
             size_hint: None, None
-            size: "600dp", "220dp"
-            pos_hint: {'center_x': 0.5, 'center_y': 0.3}
-            elevation: 1
-            padding: "10dp"
-            spacing: "10dp"
-            orientation: 'vertical'
+            size: "140dp", "48dp"
+            pos_hint: {'center_x': 0.5}
+            on_release: app.predict_phishing()
 
-            MDTextField:
-                id: url_entry
-                hint_text: "Enter URL"
-                size_hint_x: None
-                width: "250dp"
-                pos_hint: {'center_x': 0.5}
-
-            MDRaisedButton:
-                text: "Detect"
-                size_hint: None, None
-                size: "140dp", "48dp"
-                pos_hint: {'center_x': 0.5}
-                on_release: app.predict_phishing()
 
 '''
 
